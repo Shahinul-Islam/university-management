72e0043ae0292de80eea43b71eb90aad4f0afe30
shahinul islam <shahinul37@outlook.com>
2023-06-02 18:45:23 +0600

    testing huskey ckeck

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express, { Application, Request, Response, urlencoded } from "express";
-export const app: Application = express();
-import cors from "cors";
+import express, { Application, Request, Response, urlencoded } from 'express'
+export const app: Application = express()
+import cors from 'cors'
 //useing cors
-app.use(cors());
+app.use(cors())
 
 //usign parser
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
 
 //testing
-app.get("/", (req: Request, res: Response) => {
-	res.send("Hello World!");
-});
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
